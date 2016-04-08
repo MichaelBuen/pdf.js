65b66cd9c7e9f28201cbd33e6382c1f30eaabcca
MichaelBuen <me@ienablemuch.com>
2016-04-08 13:09:04 +0800

    adding dropdown

--- examples/acroforms/forms.js
+++ examples/acroforms/forms.js
@@ -57,10 +57,69 @@ function setupForm(div, content, viewport) {
     }
     element.setAttribute('style', element.getAttribute('style') + fontStyles);
   }
+  
+  
+  
+    function _extend(obj1, obj2) { // I do this because I don't want to rely on a base library like jQuery or underscore.js
+        var obj3 = {};
+        for (var prop in obj1) {
+            obj3[prop] = obj1[prop];
+        }
+        for (var prop in obj2) {
+            if (typeof(obj2[prop])!='undefined') { // don't let a rampant undefined override a value!
+                obj3[prop] = obj2[prop];
+            }
+        }
+        return obj3;
+    }
+
+  
+//   function dropDownControlfunction(itemProperties, viewport) {
+// 		var control = document.createElement('select');
+// 		if (itemProperties.multiSelect)
+// 			control.multiple=true;
+// 		control.style.width = Math.floor(itemProperties.width-3) + 'px'; // small amount + borders
+// 		control.style.height = Math.floor(itemProperties.height) + 'px'; // small amount + borders
+// 		control.style.textAlign = itemProperties.textAlignment;
+// 		if (Math.floor(itemProperties.fontSizeControl)>=Math.floor(itemProperties.height-2)) {
+// 			control.style.fontSize = Math.floor(itemProperties.height-3) + 'px';
+// 		}
+// 		else {
+// 			control.style.fontSize = itemProperties.fontSizeControl + 'px';
+// 		}
+// 		control.style.border = '1px solid #E6E6E6';
+// 		control.style.display = 'block';
+// 		if (itemProperties.options) {
+// 			for (var option in itemProperties.options) {
+// 				var optionElement = document.createElement('option');
+// 				optionElement.value = itemProperties.options[option]['value'];
+// 				optionElement.innerHTML = itemProperties.options[option]['text'];
+// 				if (typeof(itemProperties.value)=='object') { // multiple selected values. To be implemented
+
+// 				}
+// 				else if(itemProperties.value==itemProperties.options[option]['value']) {
+// 					optionElement.selected=true;
+// 				}
+// 				control.appendChild(optionElement);
+// 			}
+
+// 		}
+// 		if (itemProperties.readOnly)
+// 		{
+// 			control.disabled='disabled';
+// 			control.style.cursor = "not-allowed";
+// 		}
+// 		return control;
+// 	}
+
+  
 
   content.getAnnotations().then(function(items) {
     for (var i = 0; i < items.length; i++) {
       var item = items[i];
+      
+     
+      
       switch (item.subtype) {
         case 'Widget':
           if (item.fieldType != 'Tx' && item.fieldType != 'Btn' &&
@@ -71,8 +130,11 @@ function setupForm(div, content, viewport) {
           inputDiv.className = 'inputHint';
           div.appendChild(inputDiv);
           var input;
+          
+
+          
           if (item.fieldType == 'Tx') {
-            input = createElementWithStyle('input', item);
+            input = createElementWithStyle('input', item);            
           }
           if (item.fieldType == 'Btn') {
             input = createElementWithStyle('input', item);
@@ -89,6 +151,50 @@ function setupForm(div, content, viewport) {
           if (item.fieldType == 'Ch') {
             input = createElementWithStyle('select', item);
             // select box is not supported
+            
+            
+            // dropDownControlfunction(item, viewport)
+            
+             if (item.options) {
+                console.log('nice');
+                console.log(item.options);
+                console.log('fieldType ' + item.fieldType);
+                
+                // for(var opt in item.options) {
+                //     var optObject = item.options[opt];
+                    
+                //     var option = document.createElement("option");
+                //     option.text = optObject.text;
+                //     input.add(option);    
+                // }
+                
+                
+                var itemProperties = item;
+                var control = input;
+                if (itemProperties.options) {
+                    for (var option in itemProperties.options) {
+                        var optionElement = document.createElement('option');
+                        optionElement.value = itemProperties.options[option]['value'];
+                        optionElement.innerHTML = itemProperties.options[option]['text'];
+                        if (typeof(itemProperties.value)=='object') { // multiple selected values. To be implemented
+
+                        }
+                        else if(itemProperties.value==itemProperties.options[option]['value']) {
+                            optionElement.selected=true;
+                        }
+                        control.appendChild(optionElement);
+                    }
+
+                }
+                if (itemProperties.readOnly)
+                {
+                    control.disabled='disabled';
+                    control.style.cursor = "not-allowed";
+                }
+                
+                
+            }
+            
           }
           input.className = 'inputControl';
           input.name = item.fullName;
@@ -156,10 +262,7 @@ function renderPage(div, pdf, pageNumber, callback) {
             left : 0 
         });
 
-        console.log('ctop ' + canvasOffset.top);
-        console.log('cleft ' + canvasOffset.left);
-
-
+        
         page.getTextContent().then(function(textContent){
         var textLayer = new TextLayerBuilder({
             textLayerDiv : textLayerDiv,
@@ -171,7 +274,7 @@ function renderPage(div, pdf, pageNumber, callback) {
         console.log('text content');
         console.log( textContent);
         textLayer.render();
-        console.log('pn ' + pageNumber);
+        
     });
     
     
